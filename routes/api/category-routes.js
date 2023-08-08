64f7d6240d432c05c9d338ca60ad280433e44824
Nichole <nicholewilburn@gmail.com>
2023-08-08 13:56:19 -0500

    route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,19 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  try {
+
+    const categories = await Category.findAll();
+    res.send(categories);
+
+  } catch (err) {
+      res.status(500).json(err);
+  }
+
 });
 
 router.get('/:id', (req, res) => {
